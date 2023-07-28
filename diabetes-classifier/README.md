10ca04915dc2256a5395b132a95b4d6e213067aa
nafis-ishrak <ishrak@connect.hku.hk>
2023-07-28 07:24:39 +0000

    updated readme and app.py

---
+++ diabetes-classifier/README.md
@@ -0,0 +1,51 @@
+# Diabetes Classifier using KNN
+
+This is a simple diabetes classifier built using the K-Nearest Neighbors (KNN) algorithm. The classifier is implemented as a Streamlit app, which allows users to input their medical information and find out whether they have diabetes or not. The data is taken from [Kaggle↗](https://www.kaggle.com/datasets/uciml/pima-indians-diabetes-database).
+
+
+## Dependencies
+
+The following Python libraries are required to run this script:
+
+
+- `pickleshare==0.7.5`
+- `numpy==1.25.1`
+- `streamlit==1.25.0`
+- `pandas==2.0.3`
+
+
+## App Details
+
+The app takes in the following medical information as input:
+
+* Number of Pregnancies
+* Glucose Level
+* Blood Pressure
+* Skin Thickness
+* Insulin Level
+* BMI
+* Diabetes Pedigree Function
+* Age
+
+Using this information, the app makes a prediction on whether the user has diabetes or not. The prediction is made using a KNN model that has been trained on the Pima Indian Diabetes Dataset.
+
+
+### Deploying the model 
+
+For the deployment of this script, a Docker image is created that includes all the necessary dependencies and files, with the Dockerfile specifying the base image and installation of the required Python libraries through pip, as well as the copying of the script and pickle files into the Docker image. 
+
+On your terminal, you can pull the Docker image using the command:
+```shell
+docker pull deploifai/diabetes-classifier
+```
+
+and to run the Docker image, use the command:
+ ```shell
+ docker run -it --rm -p 8501:8501 deploifai/diabetes-classifier
+ ```
+ 
+After running the Docker image, the Diabetes classifier web application can be accessed on the local host with the port `8501`.
+
+ ```shell
+ http://localhost:8501/
+ ```
--- diabetes-classifier/app.py
+++ diabetes-classifier/app.py
@@ -8,26 +8,28 @@ model = pickle.load(open("KNN_Classifier.pkl", "rb"))
 
 # define the input fields for the user
 st.sidebar.title("Diabetes Prediction")
-pregnancies = st.sidebar.number_input("Number of Pregnancies", min_value=0, max_value=17, value=1)
-glucose = st.sidebar.number_input("Glucose Level", min_value=0, max_value=200, value=100)
-blood_pressure = st.sidebar.number_input("Blood Pressure", min_value=0, max_value=122, value=72)
-skin_thickness = st.sidebar.number_input("Skin Thickness", min_value=0, max_value=99, value=23)
-insulin = st.sidebar.number_input("Insulin Level", min_value=0, max_value=846, value=30)
-bmi = st.sidebar.number_input("BMI", min_value=0.0, max_value=67.1, value=32.0)
-diabetes_pedigree_function = st.sidebar.number_input("Diabetes Pedigree Function", min_value=0.078, max_value=2.42, value=0.3725, step=0.001)
+pregnancies = st.sidebar.number_input("Number of Pregnancies", min_value=0, max_value=17, value=1, step=1)
+glucose = st.sidebar.number_input("Glucose Level", min_value=0, max_value=200, value=100, step=1)
+blood_pressure = st.sidebar.number_input("Blood Pressure", min_value=0, max_value=122, value=72, step=1)
+skin_thickness = st.sidebar.number_input("Skin Thickness", min_value=0, max_value=99, value=23, step=1)
+insulin = st.sidebar.number_input("Insulin Level", min_value=0, max_value=846, value=30, step=1)
+bmi = st.sidebar.number_input("BMI", min_value=0.0, max_value=67.1, value=32.0, step=1.0)
+diabetes_pedigree_function = st.sidebar.number_input("Diabetes Pedigree Function", min_value=0.078, max_value=2.42, value=0.3725, step=0.01)
 age = st.sidebar.number_input("Age", min_value=21, max_value=81, value=29)
 
 
 # create a feature vector from the user inputs
 features = {
-    "Pregnancies": pregnancies,
-    "Glucose": glucose,
-    "BloodPressure": blood_pressure,
-    "SkinThickness": skin_thickness,
-    "Insulin": insulin,
-    "BMI": bmi,
-    "DiabetesPedigreeFunction": diabetes_pedigree_function,
-    "Age": age,
+    "Pregnancies": int(pregnancies),
+    "Glucose": float(glucose),
+    "BloodPressure": float(blood_pressure),
+    "SkinThickness": float(skin_thickness),
+    "Insulin": float(insulin),
+    "BMI": float(bmi),
+    "DiabetesPedigreeFunction": float(diabetes_pedigree_function),
+    "Age": int(age),
+    
+   
 }
 
 
@@ -37,12 +39,12 @@ def predict_outcome(features):
     df = pd.DataFrame(features, index=[0])
     # Make the prediction using the pre-trained model
     prediction = model.predict(df)[0]
+
     return prediction
 
 
-prediction = predict_outcome(features=features)#finding the prediction
+prediction = predict_outcome(features)#finding the prediction
 
-print(prediction)
 
 # display the prediction to the user
 st.write("")
